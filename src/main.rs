daf833fd8496803f20f4e25c69adb9dcfe65e02f
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2024-05-20 00:09:00 +0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 pub mod request;
 use http_server_starter_rust::ThreadPoll;
 use request::Request;
-use std::io::Write;
-use std::net::{TcpListener, TcpStream};
+use std::{
+    env, fs,
+    io::Write,
+    net::{TcpListener, TcpStream},
+    path::Path,
+};
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, directory: String) {
     let request = Request::new(&mut stream).unwrap();
     let response = match request.status_line.http_method.as_str() {
         "GET" => {
@@ -18,6 +22,22 @@ fn handle_connection(mut stream: TcpStream) {
                 format!(
                     "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{contents}"
                 )
+            } else if path.starts_with("/files") {
+                let params: Vec<&str> = path.split("/").collect();
+                let query = params.last().unwrap();
+                let file_path = format!("{directory}/{query}");
+                let file = Path::new(file_path.as_str());
+                let contents;
+                let response = if file.exists() {
+                    contents = fs::read_to_string(file_path).unwrap();
+                    let length = contents.len();
+                    format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {length}\r\n\r\n{contents}"
+                    )
+                } else {
+                    String::from("HTTP/1.1 404 Not Found\r\n\r\n")
+                };
+                response
             } else if path.starts_with("/user-agent") {
                 let contents = request.headers.get("User-Agent").unwrap();
                 let length = contents.len();
@@ -34,13 +54,29 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write_all(response.as_bytes()).unwrap();
 }
 fn main() {
+    let mut directory = None;
+
+    let args: Vec<String> = env::args().collect();
+    for i in 0..args.len() {
+        if args[i] == "--directory" && i + 1 < args.len() {
+            directory = Some(args[i + 1].clone());
+            break;
+        }
+    }
+    let dir = match directory {
+        Some(dir) => dir,
+        None => panic!("Dir not found"),
+    };
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     let pool = ThreadPoll::new(4);
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => pool.execute(|| {
-                handle_connection(_stream);
-            }),
+            Ok(_stream) => {
+                let dir_clone = dir.clone();
+                pool.execute(|| {
+                    handle_connection(_stream, dir_clone);
+                })
+            }
             Err(e) => {
                 println!("error: {}", e);
             }
